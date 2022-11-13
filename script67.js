8ffb727835d2b21c97a2ef23d8c6a3670e29b859
Valeryia Visheuskaya <116907996+Visheuskaya@users.noreply.github.com>
2022-11-13 11:39:21 +0100

    basic js

---
+++ script67.js
@@ -0,0 +1,23 @@
+const myMusic = [
+   {
+     "artist": "Billy Joel",
+     "title": "Piano Man",
+     "release_year": 1973,
+     "formats": [
+       "CD",
+       "8T",
+       "LP"
+     ],
+     "gold": true
+   },
+    { "artist": "Mom",
+     "title": "Pop",
+     "release_year": 2000,
+     "formats": [
+       "like",
+       "so",
+       "dislike"
+     ]
+ },
+ ];
+ 
